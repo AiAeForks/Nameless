1cf756986c1edf8ef984a8d3ff691f29da6f7989
Daniel Dimitrov <me@aiae.dev>
2026-04-28 21:12:05 +0300

    Fix parsing of IP

--- utils/requests.go
+++ utils/requests.go
@@ -1,14 +1,42 @@
 package utils
 
-import "github.com/gin-gonic/gin"
+import (
+	"net"
+	"strings"
+
+	"github.com/gin-gonic/gin"
+)
 
 // GetIpFromRequest Gets the client's proper ip address from a request.
 func GetIpFromRequest(c *gin.Context) string {
-	// Running under NGINX
-	ip := c.GetHeader("X-Forwarded-For")
+	headers := []string{
+		"CF-Connecting-IP",
+		"X-Real-IP",
+	}
 
-	if ip != "" {
-		return ip
+	for _, h := range headers {
+		if ip := strings.TrimSpace(c.GetHeader(h)); ip != "" {
+			if parsed := net.ParseIP(ip); parsed != nil {
+				return parsed.String()
+			}
+		}
+	}
+
+	if xff := c.GetHeader("X-Forwarded-For"); xff != "" {
+		ip := strings.TrimSpace(strings.Split(xff, ",")[0])
+		if parsed := net.ParseIP(ip); parsed != nil {
+			return parsed.String()
+		}
+	}
+
+	if host, _, err := net.SplitHostPort(strings.TrimSpace(c.Request.RemoteAddr)); err == nil {
+		if parsed := net.ParseIP(host); parsed != nil {
+			return parsed.String()
+		}
+	}
+
+	if parsed := net.ParseIP(strings.TrimSpace(c.Request.RemoteAddr)); parsed != nil {
+		return parsed.String()
 	}
 
 	return "::1"
